898d045cf8a3ef4f814779df2390be99035fdb4d
Tubebaum <tubebaum@gmail.com>
2012-08-28 16:16:15 -0400

    data print

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+list($encoded_sig, $payload) = explode('.', $signed_request, 2);
+$sig = base64_url_decode($encoded_sig);
+$data = json_decode(base64_url_decode($payload), true);
+print_r($data);
+?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
