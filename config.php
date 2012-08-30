4faf5bcbd07bef2a95367e3eabcfc293fb385eba
Tubebaum <tubebaum@gmail.com>
2012-08-30 17:48:00 -0400

    Adding configuration file

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+//If you are creating your own Page Tab application you must replace the values here
+//Your App ID/API Key may be found at https://developers.facebook.com/apps
+define('APP_ID', '340820556008773'); //Replace 340820556008773 with the App ID/API Key for your application
+define('PAGES', 'pages.txt'); //The name of the Page Tab database
