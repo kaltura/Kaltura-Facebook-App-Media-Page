7a1f435e2aeb5e3c9a2e588e5b272e0c3f1705fd
Tubebaum <tubebaum@gmail.com>
2012-08-30 16:18:56 -0400

    Adding a Page Tab now redirects to the Added Apps screen

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if(array_key_exists('tabs_added', $_REQUEST)) {
 	$id = array_keys($_REQUEST['tabs_added']);
 	$id = $id[0];
 	//$page = json_decode(file_get_contents('https://graph.facebook.com/'.$id))->link;
-	header('Location: '.$page.'?v=app_'.APP_ID);
+	//header('Location: '.$page.'?v=app_'.APP_ID);
 	$page = 'https://www.facebook.com/pages/edit/?id='.$id.'&sk=apps';
 	header('Location: '.$page);
 	die();
