33c2fedc22e4fff5349040f1ddbb008839d2ea37
Tubebaum <tubebaum@gmail.com>
2012-08-30 14:05:07 -0400

    Added redirect message

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ if(array_key_exists('tabs_added', $_REQUEST)) {
 	$id = array_keys($_REQUEST['tabs_added']);
 	$id = $id[0];
 	$page = json_decode(file_get_contents('https://graph.facebook.com/'.$id))->link;
+	echo 'Redirecting you to your app...';
 	echo '<script> window.location="'.$page.'?v=app_'.APP_ID.'"</script>';
 	die();
 }
